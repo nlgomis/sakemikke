365e8089b4cb000031ac5ac222227b05157aa4bf
Nil <nil@MacBook-Pro-de-Nil.local>
2024-10-31 15:25:33 +0900

    first data

---
+++ app/data/sakeData.js
@@ -0,0 +1,149 @@
+const sakeData = [
+    {
+      name: "獺祭 純米大吟醸45",
+      type: "純米大吟醸",
+      aromaCharacteristics: ["フルーティー", "華やか", "すっきり", "上品", "クリア"],
+      region: "山口県",
+      riceType: "山田錦",
+      polishingRatio: 45,
+      alcoholContent: 16,
+      sakeMeterId: null,
+      price: 2183,
+      volume: "720ml",
+      imageUrl: "",
+      taste: {
+        sweet: -20,
+        spicy: 0
+      },
+      feeling: {
+        refreshing: 20,
+        firm: 0
+      }
+    },
+    {
+      name: "久保田 純米大吟醸",
+      type: "純米大吟醸",
+      aromaCharacteristics: ["フルーティー", "華やか", "すっきり"],
+      region: "新潟県",
+      riceType: "五百万石",
+      polishingRatio: 50,
+      alcoholContent: 15,
+      sakeMeterId: 0,
+      price: 1914,
+      volume: "720ml",
+      imageUrl: "",
+      taste: {
+        sweet: -20,
+        spicy: 0
+      },
+      feeling: {
+        refreshing: 40,
+        firm: 0
+      }
+    },
+    {
+      name: "松竹梅白壁蔵「澪」",
+      type: "日本酒（発泡性）",
+      aromaCharacteristics: ["フルーティー", "甘み", "スパークリング"],
+      region: "京都府",
+      riceType: "国産米",
+      polishingRatio: 65,
+      alcoholContent: 5,
+      sakeMeterId: -70,
+      price: 1225,
+      volume: "750ml",
+      imageUrl: "",
+      taste: {
+        sweet: -90,
+        spicy: 0
+      },
+      feeling: {
+        refreshing: 65,
+        firm: 0
+      }
+    },
+    {
+      name: "醸し人九平次 純米大吟醸 山田錦 EAU DU DÉSIR",
+      type: "純米大吟醸",
+      aromaCharacteristics: ["フルーティー", "まろやか", "旨み"],
+      region: "愛知県",
+      riceType: "山田錦",
+      polishingRatio: 50,
+      alcoholContent: 16,
+      sakeMeterId: null,
+      price: 2365,
+      volume: "720ml",
+      imageUrl: "",
+      taste: {
+        sweet: -17,
+        spicy: 0
+      },
+      feeling: {
+        refreshing: 4,
+        firm: -7
+      }
+    },
+    {
+      name: "八海山 清酒",
+      type: "普通酒",
+      aromaCharacteristics: ["スッキリ辛口", "淡麗", "食中酒"],
+      region: "新潟県",
+      riceType: "五百万石、他",
+      polishingRatio: 60,
+      alcoholContent: 15.5,
+      sakeMeterId: 5,
+      price: 995,
+      volume: "720ml",
+      imageUrl: "",
+      taste: {
+        sweet: 0,
+        spicy: 40
+      },
+      feeling: {
+        refreshing: 23,
+        firm: 0
+      }
+    },
+    {
+      name: "純米吟醸 八海山 55％",
+      type: "純米吟醸",
+      aromaCharacteristics: ["綺麗", "すっきり", "ほんのり酸味"],
+      region: "新潟県",
+      riceType: "五百万石",
+      polishingRatio: 55,
+      alcoholContent: 15.5,
+      sakeMeterId: 1,
+      price: 1584,
+      volume: "720ml",
+      imageUrl: "",
+      taste: {
+        sweet: 0,
+        spicy: 20
+      },
+      feeling: {
+        refreshing: 0,
+        firm: -40
+      }
+    },
+    {
+      name: "獺祭 純米大吟醸 磨き三割九分",
+      type: "純米大吟醸",
+      aromaCharacteristics: ["華やか", "フルーティー", "上品"],
+      region: "山口県",
+      riceType: "山田錦",
+      polishingRatio: 39,
+      alcoholContent: 16,
+      sakeMeterId: null,
+      price: 2750,
+      volume: "720ml",
+      imageUrl: "",
+      taste: {
+        sweet: -20,
+        spicy: 0
+      },
+      feeling: {
+        refreshing: 65,
+        firm: 0
+      }
+    }
+  ];
