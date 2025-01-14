0d08e11bb9a2f5320918b863220dcfcc369c0ae5
toui <touiog19@gmail.com>
2025-01-14 13:43:57 +0900

    about

--- app/about/page.js
+++ app/about/page.js
@@ -31,7 +31,7 @@ const LocationTimeHeader = () => {
             <img
               src="/images/marker.png"
               alt="Location Icon"
-              className="w-5 h-5 "
+              className="w-5 h-5 mr-1"
             />
             <span>TOKYO JAPAN</span>
           </div>
